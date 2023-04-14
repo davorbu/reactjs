b0182d6b34474ad59d960e0588a6ac0db5408cbe
Davor Burmudžija <davorburmudzija@gmail.com>
2023-04-14 15:52:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import RepositoryListItem from "./Components/RepositoryListItem";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [result, setResult] = useState({});
+  const [repositories, setRepositories] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleReset = () => {
+    setRepositories([]);
+    setResult({});
+
+    setSearchTerm("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm.length > 3) {
+      fetch(`https://api.github.com/users/${searchTerm}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setResult(data);
+
+          if (data.login) {
+            fetch(`https://api.github.com/users/${data.login}/repos`)
+              .then((res) => res.json())
+              .then((data) => {
+                setRepositories(data);
+              });
+          }
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="input">
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <label
+            htmlFor="input"
+            style={{
+              display: "flex",
+              marginBottom: "25px",
+            }}
+          >
+            GitHub username:
+          </label>
+          <input
+            onChange={(e) => handleChange(e)}
+            value={searchTerm}
+            name="input"
+            type="text"
+            placeholder="e.g. facebook"
+            autoFocus={true}
+            style={{
+              marginBottom: "35px",
+              height: "3.5vh",
+            }}
+          />
+          <button
+            style={{
+              padding: "10px",
+            }}
+          >
+            GO!
+          </button>
+        </form>
+      </div>
+
+      <div
+        className="result"
+        style={{
+          display: Object.keys(result).length === 0 ? "none" : "flex",
+        }}
+      >
+        <div className="result-header">
+          <img
+            src={result.avatar_url ? result.avatar_url : "/logo192.png"}
+            alt="logo"
+            style={{
+              maxWidth: "100px",
+            }}
+          />
+          <h1>{result.name ? result.name : "Name is not provided."}</h1>
+        </div>
+        <div className="result-bio">
+          <h3>BIO:</h3>&nbsp;&nbsp;
+          <span>{result.bio ? result.bio : "Bio is not provided."}</span>
+        </div>
+        <div className="result-location">
+          <h3>LOCATION:</h3>&nbsp;&nbsp;
+          <span>
+            {result.location ? result.location : "Location is not provided."}
+          </span>
+        </div>
+        <div className="result-repositories">
+          <h3>REPOSITORIES:</h3>
+          <RepositoryListItem repositories={repositories} />
+        </div>
+
+        <button
+          onClick={handleReset}
+          style={{
+            padding: "10px",
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
